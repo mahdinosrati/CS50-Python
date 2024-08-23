1155316a71a375104738779ea3950922a7d46157
Mahdi Nosrati <82951715+mahdinosrati@users.noreply.github.com>
2024-08-23 16:50:34 +0330

    Add files via upload

---
+++ tasks/emojize/emojize.py
@@ -0,0 +1,6 @@
+from emoji import emojize
+user_input = input("Enter your text: ")
+print("Output:", emojize(user_input, language="alias"))
+
+# edited again according to the suggestion/correction here
+# https://github.com/sahinyes/CS50_Python/issues/2
